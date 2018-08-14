47b43555141a9343beed769b1220a34267d2293a
Nguyen Tuan Anh <anhngt.ce@gmail.com>
2018-08-14 12:30:02 +0700

    Add readme file

---
+++ app.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import asyncio
+import os
+
+import websockets
+
+
+async def ws_handler(websocket, path):
+    name = await websocket.recv()
+    print(f"< {name}")
+
+    greeting = f"Hello {name}!"
+
+    await websocket.send(greeting)
+    print(f"> {greeting}")
+
+
+port = int(os.getenv('PORT', 8765))  # 8765
+start_server = websockets.serve(ws_handler, '', port)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
