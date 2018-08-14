9a9f6865cd19b369f505057e9da0133f1b01d87a
Nguyen Tuan Anh <anhngt.ce@gmail.com>
2018-08-14 14:35:14 +0700

    update

--- app.py
+++ app.py
@@ -6,17 +6,25 @@ import os
 import websockets
 
 
+@asyncio.coroutine
 async def ws_handler(websocket, path):
     name = await websocket.recv()
     print(f"< {name}")
 
     greeting = f"Hello {name}!"
 
-    await websocket.send(greeting)
-    print(f"> {greeting}")
+    response = '\r\n'.join([
+        'HTTP/1.1 200 OK',
+        'Content-Type: text/json',
+        '',
+        '' + '{"version":"1.0","sessionAttributes":{},"response":{"outputSpeech":{"type":"PlainText","text":"There are 3 atm"},"shouldEndSession":true}}' + '',
+    ])
+
+    await websocket.send(response)
+    print(f"> {response}")
 
 
-port = int(os.getenv('PORT', 80))# 8765
+port = int(os.getenv('PORT', 8765))  # 8765
 start_server = websockets.serve(ws_handler, '', port)
 
 asyncio.get_event_loop().run_until_complete(start_server)
